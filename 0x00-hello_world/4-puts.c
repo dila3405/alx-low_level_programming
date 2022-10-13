3878fac633aa9bdefe80f9a1369627bd87cfd851
dila3405 <lawalmasud84@gmail.com>
2022-10-13 10:58:10 -0700

    My C prog 4-8

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *
+  *main - Print c
+  *
+  *
+  *Return: Always 0 (Confirm)
+  */
+int main(viod)
+{
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+}
