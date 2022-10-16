4fa44e316654f4396366b6f6f45456de2f0ea2fe
dila3405 <lawalmasud84@gmail.com>
2022-10-16 09:46:13 -0700

    C hello world 4, 6, and 8

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
-  *
-  * main - Print text
-  *
-  *
-  * Return: Always 0 (Confirm)
-  */
-int main(viod)
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
-#include <stdio.h>                                                                                          
+#include <stdio.h>
 
-                                                                                                            
-/**                                                                                                         
-  * main - prints the sizeof my task                                                                         
-  * 
-  * Return: Always 0 (Result)                                                                                
-  */                                                                                                         
-int main(void)                                                                                              
-{                                                                                                           
-      char a;                                                                                           
-      int b;
-      long int c;
-      long long int d;
-      float e;                                                                                  
-						                                                                                                                                             
-      printf("Size of a char: %d byte(s)\n", sizeof(a));          
-      printf("Size of an int: %d byte(s)\n", sizeof(b));                                                  
-      printf("Size of a long int: %d byte(s)\n", sizeof(c));
-      printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-      printf("Size of a float: %d byte(s)\n", sizeof(e)));
-      return (0);
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+		int a;
+		long int b;
+		long long int c;
+		char d;
+		float f;
+
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+		return (0);
 }
