1abfe0d6c532de3704f03ec915b496d93dad2d49
dila3405 <lawalmasud84@gmail.com>
2022-10-24 11:09:29 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that takes a pointer and update the value it points
+ * @n: the int to be checked
+ * written by Mdila
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	void reset_to_98("%d\n", n)
+	return (0);
+}
